fbd7c2b8f7f951b33df079589b8c7b4eb5e22fdd
Taku Kudo <taku910@users.noreply.github.com>
2018-06-19 12:42:57 +0900

    Update sentencepiece_processor.h

--- src/sentencepiece_processor.h
+++ src/sentencepiece_processor.h
@@ -145,12 +145,12 @@ class Status {
 // the argument of public APIs.
 class min_string_view {
  public:
-  constexpr min_string_view() noexcept : ptr_(nullptr), length_(0) {}
+  min_string_view() noexcept : ptr_(nullptr), length_(0) {}
   min_string_view(const std::string &str)
       : ptr_(str.data()), length_(str.size()) {}
   min_string_view(const char *str)
       : ptr_(str), length_(std::strlen(str)) {}
-  constexpr min_string_view(const char *data, size_t len)
+  min_string_view(const char *data, size_t len)
       : ptr_(data), length_(len) {}
   const char *data() const { return ptr_; }
   size_t size() const { return length_; }
