9a827fc4f59b4b25d28a0ca0fb35ced71f9f482c
chiragjn <jain.chirag925@gmail.com>
2018-07-13 19:46:04 +0530

    Add -fPIC to sentencepiece compile module to link tf op correctly

--- tensorflow/make_py_wheel.sh
+++ tensorflow/make_py_wheel.sh
@@ -78,9 +78,6 @@ build() {
   TF_CFLAGS=( $(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_compile_flags()))') )
   TF_LFLAGS=( $(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_link_flags()))') )
   
-  TF_CFLAGS=( $(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_compile_flags()))') )
-  TF_LFLAGS=( $(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_link_flags()))') )
-
   CXX_ARGS=(-std=c++11 -shared
     -I../src
     -fPIC ${TF_CFLAGS[@]} -O2
