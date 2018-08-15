5be8a97b75fa16a519cd8f05133a21db16610659
Taku Kudo <taku910@users.noreply.github.com>
2018-08-15 15:03:06 +0900

    Update make_py_wheel_mac.sh

--- tensorflow/make_py_wheel_mac.sh
+++ tensorflow/make_py_wheel_mac.sh
@@ -49,7 +49,6 @@ build() {
   sudo python get-pip.py --no-setuptools --no-wheel --ignore-installed
   pip install --upgrade setuptools
   pip install wheel
-  pip install delocate
   pip install tensorflow
 
   TF_CFLAGS=( $(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_compile_flags()))') )
@@ -70,9 +69,11 @@ build() {
   strip -x tf_sentencepiece/_sentencepiece_processor_ops.so
 
   # Builds Python manylinux wheel package.
-  PLAT_NAME=$(python -c 'import distutils.util; print(distutils.util.get_platform())')
+  # Platform name is determined by the tensorflow pip package.
+  # TODO(taku): Automatically detect the platname of tensoflow-pip
+  # PLAT_NAME=$(python -c 'import distutils.util; print(distutils.util.get_platform())')
+  PLAT_NAME=macosx_10_10_x86_64.whl
   python setup.py bdist_wheel --universal --plat-name=${PLAT_NAME}
-  delocate-addplat -x 10_10 -x 10_11 -x 10_12 dist/*.whl
   python setup.py sdist
 
   rm -fr build tf_sentencepiece.egg-info tmp
