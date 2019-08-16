0afe18beaeeb27488c3a6cad5b232e754c3b7667
Taku Kudo <taku910@users.noreply.github.com>
2019-08-16 18:50:03 +0900

    Disable tf 2.0 in mac tf

--- tensorflow/make_py_wheel_mac.sh
+++ tensorflow/make_py_wheel_mac.sh
@@ -69,7 +69,7 @@ build() {
   pip install --upgrade setuptools
   pip install wheel
 
-  build_tf_wrapper "2.0.0-beta1"
+  # build_tf_wrapper "2.0.0-beta1"
   build_tf_wrapper "1.14.0"
   build_tf_wrapper "1.13.1"
   build_tf_wrapper "1.11.0"
