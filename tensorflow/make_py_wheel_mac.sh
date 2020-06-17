02ddd110b3536e3535ec4e0a35e36d5bd4ddea2e
Taku Kudo <taku@google.com>
2020-06-17 10:25:36 +0900

    use python3 to build tf_sentencepiece on mac

--- tensorflow/make_py_wheel_mac.sh
+++ tensorflow/make_py_wheel_mac.sh
@@ -24,16 +24,16 @@ build_tf_wrapper() {
 
   # Builds _sentencepiece_processor_ops.so
   pip install tensorflow${pkg_name} --upgrade --no-cache-dir -I
-  
+
   pip uninstall numpy -y || true
   pip uninstall numpy -y || true
   pip uninstall numpy -y || true
   pip install numpy --upgrade --no-cache-dir -I
-  
+
   TF_CFLAGS=( $(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_compile_flags()))') )
   TF_LFLAGS=( $(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_link_flags()))') )
   TF_VERSION=( $(python -c 'import tensorflow as tf; print(tf.__version__)') )
-  
+
   TF_LFLAGS2=`echo -n ${TF_LFLAGS[@]} | sed -e 's/-l:lib/-l/' -e 's/.[12].dylib//'`
 
   g++ -std=c++11 -shared -undefined dynamic_lookup \
@@ -51,6 +51,30 @@ build_tf_wrapper() {
 }
 
 build() {
+  VERSION="3.7.0"
+  URL="https://www.python.org/ftp/python/3.7.0/python-3.7.0-macosx10.6.pkg"
+  INSTALL_PATH="/Library/Frameworks/Python.framework/Versions/${VERSION}/bin"
+  CURRENT_PATH=${PATH}
+
+  curl -L -o python.pkg ${URL}
+  sudo installer -pkg python.pkg -target /
+
+  if [ -f "${INSTALL_PATH}/python3" ]; then
+    ln -s ${INSTALL_PATH}/python3        ${INSTALL_PATH}/python
+    ln -s ${INSTALL_PATH}/python3-config ${INSTALL_PATH}/python-config
+    ln -s ${INSTALL_PATH}/pip3           ${INSTALL_PATH}/pip
+  fi
+
+  export PATH="${INSTALL_PATH}:${CURRENT_PATH}"
+  ls -l ${INSTALL_PATH}
+  which python
+  which pip
+  python --version
+  sudo python get-pip.py --no-setuptools --no-wheel --ignore-installed
+  pip install --upgrade setuptools
+  pip install wheel
+  pip install delocate
+
   cd tensorflow
   rm -fr build
   mkdir -p build
@@ -62,14 +86,6 @@ build() {
   make install
   cd ..
 
-  which python
-  which pip
-  python --version
-
-  curl -L -O https://bootstrap.pypa.io/get-pip.py
-  sudo python get-pip.py --no-setuptools --no-wheel --ignore-installed
-  pip install --upgrade setuptools
-  pip install wheel
   rm -f tf_sentencepiece/*.so
 
   build_tf_wrapper "2.2.0"
@@ -79,7 +95,7 @@ build() {
 #  build_tf_wrapper "1.15.2"
   build_tf_wrapper "1.15.0"
 #  build_tf_wrapper "1.14.0"
-#  build_tf_wrapper "1.13.2"  
+#  build_tf_wrapper "1.13.2"
 #  build_tf_wrapper "1.13.1"
 
   # Builds Python manylinux wheel package.
