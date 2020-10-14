0b5bd12205364c064a93c77d425ac7e6f8e41df3
Taku Kudo <taku@google.com>
2020-10-14 11:57:36 +0900

    add support py3.9 for mac

--- python/make_py_wheel_mac.sh
+++ python/make_py_wheel_mac.sh
@@ -75,7 +75,7 @@ build() {
   build_python 3.6 https://www.python.org/ftp/python/3.6.6/python-3.6.6-macosx10.6.pkg
   build_python 3.7 https://www.python.org/ftp/python/3.7.9/python-3.7.9-macosx10.9.pkg
   build_python 3.8 https://www.python.org/ftp/python/3.8.6/python-3.8.6-macosx10.9.pkg
-  build_python 3.8 https://www.python.org/ftp/python/3.9.0/python-3.9.0-macosx10.9.pkg
+  build_python 3.9 https://www.python.org/ftp/python/3.9.0/python-3.9.0-macosx10.9.pkg
 
   cd ..
 
