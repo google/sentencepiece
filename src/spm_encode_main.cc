515e65b53200c4726392d6567bec6726ea619e77
Yiyang Hao <yiyang@poolside.ai>
2023-10-25 17:00:03 +0000

    Support mixed code-text format

--- src/spm_encode_main.cc
+++ src/spm_encode_main.cc
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.!
 
+#include <cassert>
 #include <functional>
 #include <string>
 #include <vector>
@@ -92,18 +93,20 @@ int main(int argc, char *argv[]) {
   absl::flat_hash_map<std::string, int> vocab;
   sentencepiece::SentencePieceText spt;
   sentencepiece::NBestSentencePieceText nbest_spt;
-  std::function<void(absl::string_view line)> process;
+  std::function<void(absl::string_view line)> process = nullptr;
+  std::function<void(absl::string_view line, std::vector<int>& ids)> process_ex = nullptr;
   std::vector<uint32_t> sentence_sizes;
   int eos = sp.eos_id(), bos = sp.bos_id();
   char verbatim_control_char = sp.model_proto()->trainer_spec().verbatim_control_char();
   int num_threads = absl::GetFlag(FLAGS_num_threads);
   sentencepiece::ThreadPool pool(num_threads);
-  std::mutex sync;
+  std::recursive_mutex sync;
   constexpr int thread_chunk_size = 1000;
 
   const int nbest_size = absl::GetFlag(FLAGS_nbest_size);
   const float alpha = absl::GetFlag(FLAGS_alpha);
 
+  char delim = absl::GetFlag(FLAGS_new_line_delim);
   if (absl::GetFlag(FLAGS_generate_vocabulary)) {
     process = [&](absl::string_view line) {
       sentencepiece::SentencePieceText spt;
@@ -118,7 +121,8 @@ int main(int argc, char *argv[]) {
       std::vector<std::string> sps;
       CHECK_OK(sp.Encode(line, &sps));
       std::lock_guard lock(sync);
-      output->WriteLine(absl::StrJoin(sps, " "));
+      output->Write(absl::StrJoin(sps, " "));
+      output->Write(std::string(1, delim));
     };
   } else if (absl::GetFlag(FLAGS_output_format) == "id") {
     process = [&](absl::string_view line) {
@@ -128,18 +132,10 @@ int main(int argc, char *argv[]) {
       output->WriteLine(absl::StrJoin(ids, " "));
     };
   } else if (absl::GetFlag(FLAGS_output_format) == "bid") {
-    process = [&](absl::string_view line) {
-      std::vector<int> ids;
-      CHECK_OK(sp.Encode(line, &ids));
-      std::lock_guard lock(sync);
-      if (!line.empty() && line[0] == verbatim_control_char) {
-        ids.insert(ids.begin(), bos);
-      }
-      output->Write(absl::string_view(
-          reinterpret_cast<char *>(ids.data()), sizeof(int) * ids.size()));
-      output->Write(absl::string_view(
-          reinterpret_cast<char *>(&eos), sizeof(int)));
-      sentence_sizes.push_back(ids.size());
+    process_ex = [&](absl::string_view line, std::vector<int>& ids) {
+      std::vector<int> _ids;
+      CHECK_OK(sp.Encode(line, &_ids));
+      ids.insert(ids.end(), _ids.begin(), _ids.end());
     };
   } else if (absl::GetFlag(FLAGS_output_format) == "proto") {
     process = [&](absl::string_view line) { CHECK_OK(sp.Encode(line, &spt)); };
@@ -190,10 +186,122 @@ int main(int argc, char *argv[]) {
 
   std::atomic<int64_t> processed = 0;
 
-  auto processChunk = [&pool, &process, &processed](std::vector<absl::string_view>& chunk) {
-    pool.Schedule([&process, &processed, chunk](){
+  const bool isBid = absl::GetFlag(FLAGS_output_format) == "bid";
+  int ps_code_start, ps_code_end, ps_code_meta_start, ps_code_meta_end, ps_doc_end;
+  if (isBid) {
+    ps_code_start = sp.PieceToId("<0x01>");
+    ps_code_end = sp.PieceToId("<0x02>");
+    ps_code_meta_start = sp.PieceToId("<0x03>");
+    ps_code_meta_end = sp.PieceToId("<0x04>");
+    ps_doc_end = sp.PieceToId("<0x00>");
+  }
+
+  /**
+   * poolside_process will split the document into text and code blocks.
+   * 
+   * Documents with code blocks
+   * 
+   * Preprocess: Separate blocks by 0x01 and 0x02.
+   *   From:
+   *   [bytes(<some text>), 0x01, bytes(<some code>), 0x02, bytes(<some text>), 0x00]
+   *   To:
+   *   [bytes(<some text>),
+   *    0x01, bytes(<some code>),
+   *    bytes(<some text>)]
+   * 
+   *   Please note that a single document containing both NL and PL should be splitted
+   *   into multiple documents containing a single type of text (NL or PL) only.
+   *   This is due to the fact that 0x01 is only checked at the beginning of a document.
+   * 
+   * Post-process: Join blocks with 0x02, append 0x00.
+   *   From:
+   *   [bytes(<some text tokens>),
+   *    0x01, bytes(<some code tokens>),
+   *    bytes(<some text tokens>)]
+   *   To:
+   *   [bytes(<some text tokens>),
+   *    0x01, bytes(<some code tokens>), 0x02,
+   *    bytes(<some text tokens>), 0x00]
+   * 
+   * 
+   * Singular code file with metadata
+   * 
+   * Preprocess: Separate blocks by 0x01 and 0x02. Remove 0x03, 0x04.
+   *   From:
+   *   [0x03, bytes(“File path: x.cpp\n”), 0x04, 0x01, bytes(<file content>), 0x02, 0x00]
+   *   To:
+   *   [bytes(“File path: x.cpp\n”),
+   *    0x01, bytes(<file content>)]
+   * 
+   * Post process: Add 0x03, 0x04, and append the 0x00.
+   *   From:
+   *   [bytes(“File path : x . cpp \n”),
+   *    0x01, bytes(<file content tokens>)]
+   *   To:
+   *   [0x03, bytes(“File path : x . cpp \n”), 0x04,
+   *    0x01, bytes(<file content tokens>), 0x02, 0x00]
+  */
+  auto poolside_process_code_file_header = [&](absl::string_view line, std::vector<int>& ids) {
+    auto head = line.data();
+    auto tail = reinterpret_cast<const char *>(memchr(head, '\x04', line.length()));
+    assert((void("Code meta block did not end with 0x04"), tail != nullptr));
+    ids.push_back(ps_code_meta_start);
+    process_ex(absl::string_view(head + 1, tail - head), ids);
+    ids.push_back(ps_code_meta_end);
+    return tail + 1;
+  };
+
+  auto poolside_process = [&](absl::string_view line) {
+    auto head = line.data();
+    std::vector<int> ids;
+    if (*head == '\x03') {
+      head = poolside_process_code_file_header(line, ids);
+    }
+    auto end = head + line.length();
+    auto tail = head;
+    do {
+      bool hasVerbatim = *head == '\x01';
+      for (tail = hasVerbatim ? head + 1 : head; tail != end; tail++) {
+        if (*tail == '\x01' || *tail == '\x02') {
+          break;
+        }
+      }
+      assert((void("Code block did not end with 0x02"), !hasVerbatim || (tail != end && *tail == '\x02')));
+      assert((void("Text block did not end with 0x01 or 0x00"), hasVerbatim || tail == end || *tail == '\x01'));
+      if (tail == end) {
+        // Last regular text block
+        process_ex(absl::string_view(head, line.length() - (head - line.data())), ids);
+        break;
+      } else if (*tail == '\x01') {
+        // A regular text block
+        process_ex(absl::string_view(head, tail - head), ids);
+        // keep 0x01 to the next loop
+      } else { // *tail == '\x02'
+        // A code block
+        ids.push_back(ps_code_start);
+        process_ex(absl::string_view(head, tail - head), ids);
+        // write 0x02
+        ids.push_back(ps_code_end);
+        // skip 0x02
+        tail += 1;
+      }
+      head = tail;
+    } while (tail != end);
+    {
+      ids.push_back(ps_doc_end);
+      std::lock_guard lock(sync);
+      output->Write(absl::string_view(
+            reinterpret_cast<const char *>(ids.data()), sizeof(int) * ids.size()));
+    }
+    sentence_sizes.push_back(ids.size());
+  };
+
+  auto _process = isBid ? poolside_process : process;
+
+  auto processChunk = [&pool, &_process, &processed](std::vector<absl::string_view>& chunk) {
+    pool.Schedule([&_process, &processed, chunk](){
       for (auto &line : chunk) {
-        process(line);
+        _process(line);
       }
       int64_t prev = processed.fetch_add(chunk.size()) + chunk.size();
       if ((prev / thread_chunk_size) % 100 == 0) {
@@ -203,7 +311,6 @@ int main(int argc, char *argv[]) {
     chunk.clear();
   };
 
-  char delim = absl::GetFlag(FLAGS_new_line_delim);
   for (const auto &filename : rest_args) {
     if (filename.empty()) {
       LOG(FATAL) << "Pipe input is not supported. Please use --input to specify the names of the input files";
