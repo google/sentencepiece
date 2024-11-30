63d8db77ae4a4db7d9b97ba16a57d70bba22cc85
Andrei Sokol <andrei@poolside.ai>
2024-11-30 15:13:26 -0500

    Chjange min version

--- python/setup.py
+++ python/setup.py
@@ -91,7 +91,7 @@ class build_ext(_build_ext):
     # Fix compile on some versions of Mac OSX
     # See: https://github.com/neulab/xnmt/issues/199
     if sys.platform == 'darwin':
-      cflags.append('-mmacosx-version-min=10.9')
+      cflags.append('-mmacosx-version-min=14.0')
     else:
       cflags.append('-Wl,-strip-all')
       libs.append('-Wl,-strip-all')
