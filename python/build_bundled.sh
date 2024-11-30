51c17f1a6a7974aa6fa18591b52ea6a4970b8b41
Andrei Sokol <andrei@poolside.ai>
2024-11-30 00:01:04 -0500

    Update build script

--- python/build_bundled.sh
+++ python/build_bundled.sh
@@ -13,10 +13,10 @@ elif [ -f ../src/CMakeLists.txt ]; then
   SRC_DIR=..
 else
   # Try taged version. Othewise, use head.
-  git clone https://github.com/google/sentencepiece.git -b v"${VERSION}" --depth 1 || \
-  git clone https://github.com/google/sentencepiece.git --depth 1
+  git clone https://github.com/poolside/sentencepiece.git -b v"${VERSION}" --depth 1 || \
+  git clone https://github.com/poolside/sentencepiece.git --depth 1
   SRC_DIR=./sentencepiece
 fi
 
-cmake ${SRC_DIR} -B ${BUILD_DIR} -DCMAKE_BUILD_TYPE=RelWithDebInfo -DSPM_ENABLE_SHARED=OFF -DSPM_ENABLE_TCMALLOC=OFF -DSPM_ONLY_LIB=on -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}
-cmake --build ${BUILD_DIR} --config RelWithDebInfo --target install --parallel $(nproc)
+cmake ${SRC_DIR} -B ${BUILD_DIR} -DSPM_ENABLE_SHARED=OFF -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}
+cmake --build ${BUILD_DIR} --config Release --target install --parallel $(nproc)
