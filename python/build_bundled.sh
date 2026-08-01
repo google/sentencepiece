fdcc3e53740b0c7d3c76559d6c9fd2c9cae9076e
Taku Kudo <taku@google.com>
2026-08-01 01:42:58 +0000

    fixed the slow mac wheel build

--- python/build_bundled.sh
+++ python/build_bundled.sh
@@ -18,10 +18,12 @@ else
   SRC_DIR=./sentencepiece
 fi
 
+NPROC=$(nproc 2>/dev/null || sysctl -n hw.ncpu 2>/dev/null || echo 4)
 cmake ${SRC_DIR} -B ${BUILD_DIR} \
   -DSPM_ENABLE_SHARED=OFF \
   -DSPM_DISABLE_EMBEDDED_DATA=ON \
   -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} \
   -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
   -DCMAKE_CXX_FLAGS="-fPIC -fvisibility=default -ffunction-sections -fdata-sections"
-cmake --build ${BUILD_DIR} --config Release --target install --parallel $(nproc)
+cmake --build ${BUILD_DIR} --config Release --target install --parallel ${NPROC}
+
